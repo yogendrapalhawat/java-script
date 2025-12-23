dd81559ecae1746c8b1a36c5190bd90e0cd77ce5
yogendrapalhawat <yogendrapalhawat75@gmail.com>
2025-12-23 10:15:12 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,85 @@
+/*let score= "hitesh"
+
+console.log(typeof score);
+console.log(typeof(score)); //output=> String
+
+let valueInNumber= Number(score)
+console.log(valueInNumber); // output NaN
+
+ console.log(typeof valueInNumber); // output number
+*/
+/*let score= true
+
+console.log(typeof score);
+console.log(typeof(score)); //output=> boolean
+
+let valueInNumber= Number(score)
+console.log(valueInNumber); // output 1
+
+ console.log(typeof valueInNumber); // output number
+*/
+/*let score= "33abc"
+
+console.log(typeof score);
+console.log(typeof(score)); //output=> String
+
+let valueInNumber= Number(score)
+console.log(valueInNumber); // output NaN
+
+ console.log(typeof valueInNumber); // output number
+ */
+
+
+ /*
+ let score=null
+
+console.log(typeof score);
+console.log(typeof(score)); //output=> object
+
+let valueInNumber= Number(score)
+console.log(valueInNumber); // output 0
+
+ console.log(typeof valueInNumber); // output number
+
+ */
+
+ /*
+let score= undefined
+
+console.log(typeof score);
+console.log(typeof(score)); //output=> undefined
+
+let valueInNumber= Number(score)
+console.log(valueInNumber); // output NaN
+
+ console.log(typeof valueInNumber); // output number
+ */
+
+ //"33"=> 33
+ //"33abc"=>NaN
+ // true==>1 false==>0
+ // null=> 0
+
+//  let isLoggedIn =1
+//  let booleanIsLoggedIn =Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn); output => true
+
+//  let isLoggedIn =""
+//  let booleanIsLoggedIn =Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn); // output=> false
+
+//  let isLoggedIn ="yogi"
+//  let booleanIsLoggedIn =Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn); // output=> true
+
+
+ 
+// 1==>true; 0==> false
+// ""=> false
+//"yogi"=> true
+
+// string conversion
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
