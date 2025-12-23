cce79cc5fe93e7c133319ab6c9d854bf19208757
yogendrapalhawat <yogendrapalhawat75@gmail.com>
2025-12-23 08:27:52 +0000

    Added JS data type and variable examples

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use Strict"; // treat all js code as newer version 
+ // alert(3+3) // we are using nodejs, not browser
+
+ /* console.log(3
+ +3)  esa na kare readability should be high 
+ */
+
+ let name= "yogi" // string datatype
+ let age= 18
+ let isLoggedIn =false // boolean datatype
+
+ // number ==> 2 to power 53 range
+ // bigint jyada hi bada number ho
+ // string ==> ""
+ //boolean=>true/false
+ //null =>standalone value
+ //undefined=> abhi value asign nhi hui hai
+ // symbol => unique
+
+ // object
+ 
+ //console.log(typeof "yogi")==> string
+ //console.log(typeof null)==> object
+ //console.log(typeof undefined)==>undefined
---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId =144553
+let accontEmail="yogi@google.com"
+var accontPasswoard= "12345"
+accountCity = "jaipur"
+let accontState; // agar javascript me bs value diclare krke chod dete hai to uski value undefined hoti hai
+//accountId=2  // not allowed
+
+accontEmail ="hcgmail.com"
+accontPasswoard= "2222222"
+accountCity="agra"
+ console.log(accountId);
+ /*
+prefer not to use var off issue in block scope and funtional scope
+ */
+ 
+console.table([accountId,accontEmail,accontPasswoard,accountCity,accontState])
